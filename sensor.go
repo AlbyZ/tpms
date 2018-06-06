8a575d28a442972825691794e161e66d8e086e4f
AlbyZ <AlbyWRX+GiddyHub@gmail.com>
2018-06-06 21:56:01 +1000

    Update sensor.go

--- sensor.go
+++ sensor.go
@@ -17,7 +17,7 @@ func (this *Sensor) ParseData(b []byte) {
 	// Bytes 8 to 11 are pressure in kPa.
 	this.Kilopascal = int(binary.LittleEndian.Uint32(b[8:]) / 1000)
 	// Bytes 8 to 11 are pressure in kPa.
-	this.Psi = int(binary.LittleEndian.Uint32(b[8:]) / 6894.757280343134)
+	this.Psi = int(binary.LittleEndian.Uint32(b[8:]) / 6894)
 	// Bytes 12 to 15 are temperature in Celsius.
 	this.Celsius = int(binary.LittleEndian.Uint32(b[12:]) / 100)
 }
