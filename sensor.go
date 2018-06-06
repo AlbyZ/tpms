ae8041b2e1472e46499255ddcfacf1970d00223f
AlbyZ <AlbyWRX+GiddyHub@gmail.com>
2018-06-06 21:46:35 +1000

    Added PSI

--- sensor.go
+++ sensor.go
@@ -9,12 +9,15 @@ type Sensor struct {
 	Id         int
 	Address    ble.Addr
 	Kilopascal int
+	Psi int
 	Celsius    int
 }
 
 func (this *Sensor) ParseData(b []byte) {
 	// Bytes 8 to 11 are pressure in kPa.
 	this.Kilopascal = int(binary.LittleEndian.Uint32(b[8:]) / 1000)
+	// Bytes 8 to 11 are pressure in kPa.
+	this.Psi = int(binary.LittleEndian.Uint32(b[8:]) / 6894.757280343134)
 	// Bytes 12 to 15 are temperature in Celsius.
 	this.Celsius = int(binary.LittleEndian.Uint32(b[12:]) / 100)
 }
